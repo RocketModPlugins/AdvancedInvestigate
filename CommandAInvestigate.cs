c906538044b28dec5718246d89886ab6cdf5d0d8
HarryW123 <harrywilliams2408@gmail.com>
2017-03-16 22:15:27 +0000

    Upload

---
+++ CommandAInvestigate.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using Rocket.Unturned;
+using Rocket.Unturned.Commands;
+using Rocket.Unturned.Chat;
+using System.Collections.Generic;
+using SDG.Unturned;
+
+namespace AdvancedInvestigate
+{
+    public class CommandAInvestigate : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller
+        {
+            get { return AllowedCaller.Both; }
+        }
+
+        public string Name
+        {
+            get { return "advancedinvestigate"; }
+        }
+
+        public string Help
+        {
+            get { return "This Plugin Gives Advanced Details that /investigate."; }
+        }
+
+        public string Syntax
+        {
+            get { return "<player>"; }
+        }
+
+        public List<string> Aliases
+        {
+            get { return new List<string>() { "ainvestigate", "ainv" }; }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer victim = UnturnedPlayer.FromName(command[0]);
+
+            UnturnedChat.Say(caller, "Player: " + victim.CharacterName, Color.red);
+            UnturnedChat.Say(caller, "SteamID64: " + victim.CSteamID, Color.red);
+            UnturnedChat.Say(caller, "IP Address: " + AdvancedInvestigate.Main.GetIP(victim.CSteamID), Color.red);
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string>
+              {
+                  "advanced.investigate"
+              };
+            }
+        }
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,39 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using Steamworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdvancedInvestigate
+{
+    public class Main : RocketPlugin
+    {
+        public static Main Instance;
+
+        protected override void Load()
+        {
+            Instance = this;
+
+            Logger.Log("LAODADEGFEF");
+        }
+
+        protected override void Unload()
+        {
+
+        }
+
+        private void FixedUpdate()
+        {
+        }
+
+        public static string GetIP(CSteamID id)
+        {
+            P2PSessionState_t sessionState;
+            SteamGameServerNetworking.GetP2PSessionState(id, out sessionState);
+            return Parser.getIPFromUInt32(sessionState.m_nRemoteIP);
+        }
+    }
+}
